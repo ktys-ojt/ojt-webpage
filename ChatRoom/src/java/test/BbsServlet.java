965830b819690dfe24de931040aa4f8a4482c526
k_yoshida <k_yoshida@C88476.jdcsv01.okinawa.javacenter>
2016-06-17 10:14:40 +0900

    コメントの追加

--- ChatRoom/src/java/test/BbsServlet.java
+++ ChatRoom/src/java/test/BbsServlet.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package test;
 
 import java.io.IOException;
@@ -35,34 +30,45 @@ public class BbsServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        //request.setCharacterEncoding("Shift_JIS");
-        ServletContext context = getServletContext();
-        List<BbsBean> messageList = (List<BbsBean>) context.getAttribute("beans");
+        //アプリケーション属性のメッセージリストを取得
+        ServletContext application = getServletContext();
+        List<BbsBean> messageList = (List<BbsBean>) application.getAttribute("beans");
+        //まだない場合新規に作成
         if (messageList == null) {
             messageList = new ArrayList<>();
         }
+
+        //インスタンス作成
         BbsBean bean = new BbsBean();
         List<ErrorBean> errorList = new ArrayList<>();
 
+        //リクエストを取得
         String name = request.getParameter("name");
         String comment = request.getParameter("comment");
         String dateTimeFormat = "yyyy/MM/dd HH:mm:ss";
 
-        errorCheck("name", name, bean, errorList);
+        //beanに格納
         bean.setName(name);
         LocalDateTime dateTime = LocalDateTime.now();
         bean.setDateTime(dateTime.
                 format(DateTimeFormatter.ofPattern(dateTimeFormat)));
-        errorCheck("comment", comment, bean, errorList);
-        bean.setComment(comment);
+        bean.setComment(comment.replaceAll("\n", "<br>"));
 
+        //入力エラーがないかチェックする
+        //そのうちjQueryに任せる
+        errorCheck("name", name, bean, errorList);
+        errorCheck("comment", comment, bean, errorList);
+
+        //エラーリストに要素がなければメッセージに格納
+        //あればエラーとしてレスポンス(リクエストとして)を返す
         if (errorList.isEmpty()) {
             messageList.add(bean);
-            context.setAttribute("beans", messageList);
+            application.setAttribute("beans", messageList);
         } else {
             request.setAttribute("error", errorList);
         }
 
+        //トップ画面にフォワード(リダイレクトのほうがいいか？)
         RequestDispatcher dispatcher = request.
                 getRequestDispatcher("/input.jsp");
         dispatcher.forward(request, response);
